147f9475d9cb7ae7dcf4eb0b3a65b837eedbbc8c
nabinpantha <panthanabin@gmail.com>
2021-08-12 00:53:53 -0500

    first commit

--- src/main/java/edu/application/project/university/CollegeApplication.java
+++ src/main/java/edu/application/project/university/CollegeApplication.java
@@ -8,6 +8,8 @@ public class CollegeApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CollegeApplication.class, args);
+		// i like java
+		
 		// it was a nice experience
 	}
 
